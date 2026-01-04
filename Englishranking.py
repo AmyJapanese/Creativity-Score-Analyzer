f0aaf79cbf0af7ce628e476e2a8cf69dddf16b63
AmyJapanese <onda-rio@proton.me>
2026-01-05 00:10:53 +0900

    ver1.2

---
+++ Englishranking.py
@@ -0,0 +1,12 @@
+from wordfreq import iter_wordlist
+
+def get_word_rank(word, lang='en', wordlist='best'):
+    for rank, w in enumerate(iter_wordlist(lang, wordlist), start=1):
+        if w == word:
+            return rank
+    return "リストにありませんでした。"  # リストにないとき
+
+# 例
+while True:
+    search_word = input("順位を知りたい単語を入力してください：")
+    print(get_word_rank(search_word, "en"))
